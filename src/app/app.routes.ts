ad9839ee48425afb969fb8feac7fbd7f9214c5d3
Developer <developer@example.com>
2026-07-22 15:40:20 +0530

    Configure application routing and core shell layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
+  },
+  {
+    path: 'jobs',
+    loadComponent: () => import('./features/jobs/jobs.component').then(m => m.JobsComponent),
+  },
+  {
+    path: 'jobs/:id',
+    loadComponent: () => import('./features/job-details/job-details.component').then(m => m.JobDetailsComponent),
+  },
+  {
+    path: 'apply/:id',
+    loadComponent: () => import('./features/apply/apply.component').then(m => m.ApplyComponent),
+  },
+  {
+    path: 'thank-you',
+    loadComponent: () => import('./features/thank-you/thank-you.component').then(m => m.ThankYouComponent),
+  },
+  {
+    path: '404',
+    loadComponent: () => import('./features/not-found/not-found.component').then(m => m.NotFoundComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
